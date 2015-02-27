d38b79e2c4a0b062ace38d0afe0bd81f18713999
Boris <zhouboli@gmail.com>
2015-02-27 14:42:55 +0800

    Initial Commit

---
+++ Memo Pad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Memo Pad
+//
+//  Created by Boris Chou on 2/27/15.
+//  Copyright (c) 2015 BorisApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Memo Pad/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Memo Pad
+//
+//  Created by Boris Chou on 2/27/15.
+//  Copyright (c) 2015 BorisApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
